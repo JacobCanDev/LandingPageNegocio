78aa7c27187334ac6ecd5fe3e4e0f92ae73fbec3
JacobCanDev <154299767+JacobCanDev@users.noreply.github.com>
2023-12-22 16:02:11 -0600

    Routing simple
    Routing simple

--- src/App.js
+++ src/App.js
@@ -5,17 +5,33 @@ import Work from "./Components/Work";
 import Testimonial from "./Components/Testimonial";
 import Contact from "./Components/Contact";
 import Footer from "./Components/Footer";
-
+import {Routes,Route} from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-      <Home />
-      <About />
-      <Work />
-      <Testimonial />
-      <Contact />
-      <Footer />
-    </div>
+
+    <Routes>
+      <Route
+        path='/'
+        element={<Home />}
+      />
+       <Route
+        path='/About'
+        element={<About />}
+      />
+       <Route
+        path='/Contact'
+        element={<Contact />}
+      />
+
+    </Routes>
+    // <div className="App">
+    //   <Home />
+    //   <About />
+    //   <Work />
+    //   <Testimonial />
+    //   <Contact />
+    //   <Footer />
+    // </div>
   );
 }
 
