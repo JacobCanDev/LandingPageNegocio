2956988fc5c6ff48185cf9c97c92b5d9970a3d04
JacobCanDev <154299767+JacobCanDev@users.noreply.github.com>
2024-04-06 12:29:22 -0600

    ajustes home

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,14 +2,13 @@ import React from "react";
 import BannerBackground from "../Assets/home-banner-background.png";
 import BannerImage from "../Assets/home-banner-image.png";
 import Navbar from "./Navbar";
-import { FiArrowRight } from "react-icons/fi";
-import Carousel from "react-material-ui-carousel";
 import Item from "./Item";
 import Footer from "./Footer";
 import { Grid, Typography } from "@mui/material";
 import products from "../Helpers/Products/products.json";
 import { configurations } from "../Configurations/enums";
 import { Fragment } from "react";
+import CarouselComponent from "./Utils/CarouselComponent";
 const Home = () => {
   return (
     <Fragment>
@@ -19,19 +18,7 @@ const Home = () => {
         </Grid>
 
         <Grid item xs={12}>
-          <Carousel
-            fullHeightHover={false}
-            navButtonsProps={{
-              style: {
-                backgroundColor: configurations.companyColor,
-                borderRadius: "50,50",
-              },
-            }}
-          >
-            {products.map((item) => (
-              <Item key={item.id} item={item} />
-            ))}
-          </Carousel>
+          <CarouselComponent products={products} />
         </Grid>
       </Grid>
     </Fragment>
