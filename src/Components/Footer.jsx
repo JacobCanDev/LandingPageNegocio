2e67b55ee24a2f7c129aad63ca87ed7dad021735
JacobCanDev <154299767+JacobCanDev@users.noreply.github.com>
2024-04-06 20:28:16 -0600

    Ajustes diseño y enums nuevos

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,39 +4,105 @@ import { BsTwitter } from "react-icons/bs";
 import { SiLinkedin } from "react-icons/si";
 import { BsYoutube } from "react-icons/bs";
 import { FaFacebookF } from "react-icons/fa";
-
+import { Copyright } from "@mui/icons-material";
+import { Typography, Grid } from "@mui/material";
+import { Fragment } from "react";
+import { Link } from "react-router-dom";
+import {
+  navbarSectionsNames,
+  routes,
+  companyData,
+} from "../Configurations/enums";
 const Footer = (props) => {
   return (
-    <div className="footer-wrapper">
-      <div className="footer-section-one">
-        <div className="footer-logo-container">
-          <img src={Logo2} alt="" />
+    <Fragment>
+      <Grid item xs={12}>
+        <div className="footer-wrapper">
+          <div className="footer-section-one">
+            <div className="footer-logo-container">
+              <img src={Logo2} alt="" />
+            </div>
+            <div className="footer-icons">
+              <BsTwitter />
+              <SiLinkedin />
+              <BsYoutube />
+              <FaFacebookF />
+            </div>
+          </div>
+          <div className="footer-section-two">
+            <Grid xs={12}>
+              <Link
+                to={routes.HomeRoute}
+                component="button"
+                variant="body2"
+                onClick={() => {
+                  console.info("I'm a button.");
+                }}
+                style={{ textDecoration: "none", color: "black" }}
+              >
+                <Typography>{navbarSectionsNames.HomeLabel}</Typography>
+              </Link>
+              <Link
+                to={routes.AboutRoute}
+                component="button"
+                variant="body2"
+                onClick={() => {
+                  console.info("I'm a button.");
+                }}
+                style={{ textDecoration: "none", color: "black" }}
+              >
+                <Typography>{navbarSectionsNames.AboutLabel}</Typography>
+              </Link>
+              <Link
+                to={routes.ProductsRoute}
+                component="button"
+                variant="body2"
+                onClick={() => {
+                  console.info("I'm a button.");
+                }}
+                style={{ textDecoration: "none", color: "black" }}
+              >
+                <Typography>{navbarSectionsNames.OursProductsLabel}</Typography>
+              </Link>
+              <Link
+                to={routes.ClientsRoute}
+                component="button"
+                variant="body2"
+                onClick={() => {
+                  console.info("I'm a button.");
+                }}
+                style={{ textDecoration: "none", color: "black" }}
+              >
+                <Typography>{navbarSectionsNames.OursClients}</Typography>
+              </Link>
+              <Link
+                to={routes.ContactRoute}
+                component="button"
+                variant="body2"
+                onClick={() => {
+                  console.info("I'm a button.");
+                }}
+                style={{ textDecoration: "none", color: "black" }}
+              >
+                <Typography>{navbarSectionsNames.ContactUsLabel}</Typography>
+              </Link>
+            </Grid>
+
+            <Grid xs={12}>
+              <Typography>{companyData.PhoneNumber}</Typography>
+              <Typography>{companyData.Email}</Typography>
+              <Typography>{companyData.Address}</Typography>
+            </Grid>
+          </div>
         </div>
-        <div className="footer-icons">
-          <BsTwitter />
-          <SiLinkedin />
-          <BsYoutube />
-          <FaFacebookF />
-        </div>
-      </div>
-      <div className="footer-section-two">
-        <div className="footer-section-columns">
-          <span>Qualtiy</span>
-          <span>Help</span>
-          <span>Share</span>
-          <span>Carrers</span>
-          <span>Testimonials</span>
-          <span>Work</span>
-        </div>
-        <div className="footer-section-columns">
-          <span>(999) 129-5603</span>
-          <span>mrejon@comercializadoraremm.com</span>
-          <span>
-            C.15 #166A X 12 Y 4 DIAG. MONTECRISTO C.P. 97133 MÉRIDA,YUCATÁN
-          </span>
-        </div>
-      </div>
-    </div>
+      </Grid>
+
+      <Grid item xs={12}>
+        <Typography variant="caption" color="black">
+          &copy; {new Date().getFullYear()} {companyData.Name}
+        </Typography>
+      </Grid>
+    </Fragment>
   );
 };
 
